7151b9480a108345f53fa223ddc2863fb3badb77
IMRinat <shamrinat@mail.ru>
2022-07-22 21:29:24 +0500

    endChat work now

--- Zadanie13ПА/src/chat/ChatModule.java
+++ Zadanie13ПА/src/chat/ChatModule.java
@@ -1,18 +1,8 @@
 package chat;
 
 public class ChatModule {
-  private Boolean endChat;
+  public static Boolean endChat;
   
-  public Boolean getEndChat() {
-    return endChat;
-  }
-
-  public void setEndChat(Boolean endChat) {
-    this.endChat = endChat;
-  }
-
-
-
   public ChatModule(){
     endChat = false;
     ReadConsoleModule readConsoleModule = new ReadConsoleModule(this);
