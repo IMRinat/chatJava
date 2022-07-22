b75c56a0497f7db9e6b1006a9a3e9e907dcf0781
IMRinat <shamrinat@mail.ru>
2022-07-22 21:11:08 +0500

    fix

--- Zadanie13ПА/src/chat/ChatModule.java
+++ Zadanie13ПА/src/chat/ChatModule.java
@@ -1,14 +1,26 @@
 package chat;
 
 public class ChatModule {
+  private Boolean endChat;
   
+  public Boolean getEndChat() {
+    return endChat;
+  }
+
+  public void setEndChat(Boolean endChat) {
+    this.endChat = endChat;
+  }
+
+
+
   public ChatModule(){
-    ReadConsoleModule readConsoleModule = new ReadConsoleModule();
+    endChat = false;
+    ReadConsoleModule readConsoleModule = new ReadConsoleModule(this);
     readConsoleModule.init();
     readConsoleModule.start();
     
     ReadHistoryModule readHistoryModule = new ReadHistoryModule();
-    readHistoryModule.init(readConsoleModule.getUserName());
+    readHistoryModule.init(readConsoleModule.getUserName(),this);
     readHistoryModule.start();
   }
 
